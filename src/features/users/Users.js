71447dd264b29ec365b3d107d6ffbd2957566302
Leandro Dos Santos Afonso <leandro.afonso@hotmail.com>
2022-09-11 23:08:21 -0400

    Completed Assignment

--- src/features/users/Users.js
+++ src/features/users/Users.js
@@ -1,16 +1,20 @@
 import React from "react";
+import {useSelector} from "react-redux"
 // add any needed imports here
 
 function Users() {
-  return (
-    <div>
-      <ul>
-        Users!
-        {/* Write code here that displays the usernames of all users in the Redux store */}
-        {/* In addition, display the total number of users curently in the store */}
-      </ul>
-    </div>
-  );
+  const state = useSelector(state => state.users)  
+  console.log(state);
+   return (
+     <div>
+       <ul>
+         Users!
+         {state.map((user)=><li>{user.username}</li>)}
+        
+       </ul>
+       <p>{`Total Users: ${state.length}`}</p>
+     </div>
+   );
 }
 
 export default Users;
